8d8cafecea2ffecf91a6505a9291cfb06c5046ee
Viraj-M <rajashreemarathe80@gmail.com>
2022-12-23 13:12:42 +0530

    blogs

--- server.js
+++ server.js
@@ -7,8 +7,21 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/about', (req, res) => {
-  res.sendFile(__dirname + '/about.html');
+
+app.get('/blog1', (req, res) => {
+  res.sendFile(__dirname + '/blogs/blog1.html');
+});
+
+app.get('/blog2', (req, res) => {
+  res.sendFile(__dirname + '/blogs/blog2.html');
+});
+
+app.get('/blog3', (req, res) => {
+  res.sendFile(__dirname + '/blogs/blog3.html');
+});
+
+app.get('/blog4', (req, res) => {
+  res.sendFile(__dirname + '/blogs/blog4.html');
 });
 
 app.listen(3000, () => {
